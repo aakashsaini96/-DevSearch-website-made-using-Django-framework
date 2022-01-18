dbb7183f34cee965414d0f3412b7229cd9076916
Aakash Saini <aakashsaini.person@gmail.com>
2022-01-18 12:04:07 +0530

    Added env variables

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -126,9 +126,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'devsearch',
-        'USER': 'aakash',
-        'PASSWORD': 'meatpg66',
-        'HOST': 'database-1.c0fxl4zblska.us-east-1.rds.amazonaws.com',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': '5432',
     }
 }
@@ -178,8 +178,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aakashsaini.person@gmail.com'
-EMAIL_HOST_PASSWORD = 'pjrdxddghtltejed'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 # Static files (CSS, JavaScript, Images)
@@ -209,9 +209,9 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
 
-AWS_ACCESS_KEY_ID = 'AKIA56ZCMKRTCE2JYLXQ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'devsearch-bucket-aakash'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 
 if os.getcwd() == '/app':
